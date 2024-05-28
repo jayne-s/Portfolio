835428d9876107c4c1f36b4422bf25d420aa76d0
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2024-05-28 01:00:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function getNetwork(){
+  window.open("https://github.com/jayne-s/Network_Simulation");
+}
+
+function getProcessor(){
+  window.open("https://github.com/jayne-s/Single_Cycle_Processor");
+}
+
+function getFintech(){
+  window.open("https://github.com/jayne-s/Fintech_Focus");
+}
+
+function getALU(){
+  window.open("https://github.com/jayne-s/Arithmetic_Logic_Unit/tree/main");
+}
